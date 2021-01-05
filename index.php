c477130d2d062540d958b63deaf1f856494b0a9f
Michael Pellegrini <mipelleg@microsoft.com>
2021-01-05 13:16:12 -0800

    Adds material icons stylesheet

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>Michael Pellegrini's Web Development Blog</title>
     <link rel="stylesheet" href="./styles/app.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif] -->
 </head>
 <body>
-    <h1>Hello world</h1>
+    <h1>Hello world <i class="material-icons">face</i></h1>
 
 </body>
 </html>
