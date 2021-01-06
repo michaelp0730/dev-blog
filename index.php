dd6d9398357a0beedca976d90b3a3c823373a579
Michael Pellegrini <mipelleg@microsoft.com>
2021-01-05 16:03:44 -0800

    General page styling

--- index.php
+++ index.php
@@ -11,7 +11,23 @@
     <![endif] -->
 </head>
 <body>
-    <h1>Hello world <i class="material-icons">face</i></h1>
-
+    <div class="wrapper">
+        <h1>Hello world <i class="material-icons">face</i></h1>
+        <h3>Some glorious subtitle about my site</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim velit magna, a eleifend arcu rhoncus id. Mauris tempus magna sed libero lacinia, a mollis justo faucibus. Mauris malesuada ultricies ligula, vel congue metus suscipit vel. Aliquam tempor mi convallis, feugiat ante et, suscipit nisl. Ut viverra, odio vel facilisis feugiat, metus orci ultrices lectus, sed commodo eros risus eu libero. Nullam efficitur purus quis urna vestibulum, iaculis aliquet dui ornare. Sed viverra laoreet nunc, eget vehicula dolor condimentum sit amet. Donec lobortis rhoncus nulla, at tincidunt ipsum pulvinar non. Morbi quis vestibulum risus, sed consectetur nisl. Nullam sagittis nisl vel orci gravida, sit amet vestibulum ex tristique. Praesent metus ligula, pharetra sed tortor volutpat, rutrum finibus nisl. Sed id odio vestibulum, pretium dui eu, aliquet urna.</p>
+        <ul>
+            <li>First list item</li>
+            <li>Second list item</li>
+            <li>Third list item</li>
+        </ul>
+        <p>Ut sit amet feugiat erat. Aliquam erat volutpat. Maecenas porttitor risus velit, sed volutpat neque tristique eu. Cras luctus turpis tortor, nec congue elit vestibulum sit amet. Praesent vitae varius sapien. Cras pellentesque, ipsum sit amet consectetur sagittis, ex lacus tempor ante, ut fringilla sapien ipsum et dui. Vivamus aliquam, tortor sed sodales mattis, eros ligula volutpat ante, at faucibus libero ex eu ligula. Maecenas fringilla elit sit amet enim eleifend tempus.</p>
+        <pre>
+            <xmp>
+<section>
+    <h2>Heading for the section</h2>
+    <p>Curabitur vel <a href="https://google.com">erat vitae</a> ligula blandit finibus id ac justo. Proin id luctus dui. In mattis lorem at risus egestas porta. Maecenas porttitor laoreet orci vitae convallis.</p>
+</section></xmp>
+        </pre>
+    </div>
 </body>
 </html>
